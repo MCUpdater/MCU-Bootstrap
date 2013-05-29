51060a566239d55f8e7b00cae65b1d62ca663985
Scott Barbour <smbarbour@gmail.com>
2013-05-29 12:42:18 -0500

    Initial commit

---
+++ src/org/smbarbour/mcu/BootstrapForm.java
@@ -0,0 +1,135 @@
+package org.smbarbour.mcu;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JWindow;
+import javax.swing.SwingConstants;
+import javax.swing.JProgressBar;
+import javax.swing.UIManager;
+
+public class BootstrapForm extends JWindow {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8042232921626373270L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+					BootstrapForm frame = new BootstrapForm();
+					frame.setLocationRelativeTo( null );
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public BootstrapForm() {
+		// setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel progressPanel = new JPanel();
+		progressPanel.setBorder(new EmptyBorder(3, 0, 0, 0));
+		contentPane.add(progressPanel, BorderLayout.SOUTH);
+		progressPanel.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblStatus = new JLabel("Preparing MCUpdater");
+		lblStatus.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.exit(0);
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+			}
+			
+		});
+		progressPanel.add(lblStatus, BorderLayout.SOUTH);
+		
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setValue(65);
+		progressPanel.add(progressBar);
+		
+		JPanel logoPanel = new JPanel() {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 8686753828984892019L;
+			ImageIcon image = new ImageIcon(BootstrapForm.class.getResource("/org/smbarbour/mcu/bg_main.png"));
+			
+			@Override
+			protected void paintComponent(Graphics g) {
+				Image source = this.image.getImage();
+				int w = source.getWidth(null);
+				int h = source.getHeight(null);
+				BufferedImage image = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+				Graphics2D g2d = (Graphics2D)image.getGraphics();
+				g2d.drawImage(source, 0, 0, null);
+				g2d.dispose();
+		        int width = getWidth();  
+		        int height = getHeight();  
+		        int imageW = image.getWidth(this);  
+		        int imageH = image.getHeight(this);  
+		   
+		        // Tile the image to fill our area.  
+		        for (int x = 0; x < width; x += imageW) {  
+		            for (int y = 0; y < height; y += imageH) {  
+		                g.drawImage(image, x, y, this);  
+		            }  
+		        }  
+			}
+			
+			
+		};
+		contentPane.add(logoPanel, BorderLayout.CENTER);
+		logoPanel.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblLogo = new JLabel("");
+		lblLogo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLogo.setIcon(new ImageIcon(BootstrapForm.class.getResource("/org/smbarbour/mcu/mcu-logo.png")));
+		logoPanel.add(lblLogo, BorderLayout.CENTER);
+		
+		setSize(480, 290);
+	}
+
+}
